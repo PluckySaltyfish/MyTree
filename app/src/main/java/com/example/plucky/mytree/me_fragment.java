a1b7077f05748cbb98457b90a4bc1e28a65eb1eb
plucky <1828151761@qq.com>
2017-10-28 11:45:02 +0800

    before recycler test

--- app/src/main/java/com/example/plucky/mytree/me_fragment.java
+++ app/src/main/java/com/example/plucky/mytree/me_fragment.java
@@ -7,14 +7,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.LineChart;
+
 public class me_fragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.me_fragment, container, false);
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.me_fragment,container,false);
+        LineChart LineChart = (LineChart)v.findViewById(R.id.task_category);
+        PairData[] dataObjects ={new PairData(0,30),new PairData(1,99),new PairData(2,22),
+                new PairData(3,44),new PairData(4,35),new PairData(5,60),new PairData(6,80)};
+
+        SemiAnnualChart wkChart = new SemiAnnualChart(LineChart,dataObjects);
+        wkChart.setData();
+        wkChart.drawChart();
+        return v;
     }
 
 }
